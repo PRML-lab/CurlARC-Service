21d3c7b28732f6f41ab7edace869b037e7628908
dyoshyy <d.yoshhyy@gmail.com>
2024-05-10 00:43:36 +0900

    build an env

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func main() {
+// 	if err := run(context.Background()); err != nil {
+// 		slog.Error("failed to terminated server", "error", err)
+// 		os.Exit(1)
+// 	}
+// }
+
+// func run(ctx context.Context) error {
+// 	if err := config.Init(); err != nil {
+// 		return err
+// 	}
+
+// 	srv := server.NewServer()
+// 	return srv.Run(ctx)
+// }
+
+func main() {
+	fmt.Println("Hello, World!")
+}
