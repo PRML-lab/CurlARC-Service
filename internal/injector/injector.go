70ee9499f873a84fcf11a303cd7d0a1da3f11537
dyoshyy <d.yoshhyy@gmail.com>
2024-06-24 23:39:24 +0900

    feat: injectorの実装

--- internal/injector/injector.go
+++ internal/injector/injector.go
@@ -1,1 +1,30 @@
 package injector
+
+import (
+	"CurlARC/internal/domain/repository"
+	"CurlARC/internal/handler"
+	"CurlARC/internal/infra"
+	"CurlARC/internal/usecase"
+)
+
+func InjectDB() infra.SqlHandler {
+	sqlhandler := infra.NewSqlHandler()
+	return *sqlhandler
+}
+
+// UserRepository (interface) に実装である SqlHandler を渡し生成する
+
+func InjectUserRepository() repository.UserRepository {
+	sqlHandler := InjectDB()
+	return infra.NewUserRepository(sqlHandler)
+}
+
+func InjectUserUsecase() usecase.UserUsecase {
+	userRepo := InjectUserRepository()
+	return usecase.NewUserUsecase(userRepo)
+}
+
+func InjectUserHandler() handler.UserHandler {
+	userUsecase := InjectUserUsecase()
+	return handler.NewUserHandler(userUsecase)
+}
