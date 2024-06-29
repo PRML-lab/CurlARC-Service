d1a99389c448d5898b45bdce36434ecd4ec22854
dyoshyy <d.yoshhyy@gmail.com>
2024-06-29 11:54:25 +0900

    fix: user model の teamIdを配列から一つに修正（後に対応する予定）

--- internal/domain/model/user.go
+++ internal/domain/model/user.go
@@ -1,10 +1,11 @@
 package model
 
 type User struct {
-	Id      string   `json:"id"`
-	Name    string   `json:"name"`
-	Email   string   `json:"email"`
-	TeamIds []string `json:"team_ids"`
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	// TeamIds []string `json:"team_ids"` // 配列だとGORMの対応がめんどくさいので一旦teamIdは１つだけにする
+	TeamIds string `json:"team_ids"`
 }
 
 type Users []User
