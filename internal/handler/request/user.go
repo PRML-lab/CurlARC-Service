e9f8c2b8fc8af442ebb29eb39e1d925d02e3a8d5
dyoshyy <d.yoshhyy@gmail.com>
2024-07-13 11:32:48 +0900

    refac: req resの切り出し

---
+++ internal/handler/request/user.go
@@ -0,0 +1,28 @@
+package request
+
+import "github.com/lib/pq"
+
+type SignUpRequest struct {
+	IdToken string `json:"id_token"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+}
+
+type SignInRequest struct {
+	IdToken string `json:"id_token"`
+}
+
+type GetUserRequest struct {
+	Id string `json:"id"`
+}
+
+type UpdateUserRequest struct {
+	Id      string         `json:"id"`
+	Name    string         `json:"name"`
+	Email   string         `json:"email"`
+	TeamIds pq.StringArray `json:"team_ids"`
+}
+
+type DeleteUserRequest struct {
+	Id string `json:"id"`
+}
