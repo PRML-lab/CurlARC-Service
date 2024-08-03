90c35702b64b06cf5ee2d4935a202e9f234b473c
dyoshy <d.yoshhyy@gmail.com>
2024-08-03 18:03:39 +0900

    feat: jwtをsetcookie

--- internal/handler/response/user.go
+++ internal/handler/response/user.go
@@ -1,12 +1,5 @@
 package response
 
-type SignInResponse struct {
-	Jwt   string `json:"jwt"`
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
 type GetUserResponse struct {
 	Id    string `json:"id"`
 	Name  string `json:"name"`
