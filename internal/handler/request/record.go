4a62955202acc1642ec414c493f90550883f78ca
dyoshy <d.yoshhyy@gmail.com>
2024-09-08 15:23:41 +0900

    update: request types

--- internal/handler/request/record.go
+++ internal/handler/request/record.go
@@ -1,28 +1,28 @@
 package request
 
 import (
-	"CurlARC/internal/domain/model"
+	entity "CurlARC/internal/domain/entity/record"
 	"time"
 )
 
 type CreateRecordRequest struct {
-	Result        model.Result `json:"result"`
-	EnemyTeamName string       `json:"enemy_team_name"`
-	Place         string       `json:"place"`
-	Date          time.Time    `json:"date"`
+	Result        entity.Result `json:"result"`
+	EnemyTeamName string        `json:"enemy_team_name"`
+	Place         string        `json:"place"`
+	Date          time.Time     `json:"date"`
 }
 
 type AppendEndDataRequest struct {
-	EndsData []model.DataPerEnd `json:"ends_data"`
+	EndsData []entity.DataPerEnd `json:"ends_data"`
 }
 
 type UpdateRecordRequest = struct {
-	Result        *model.Result       `json:"result"`
-	EnemyTeamName *string             `json:"enemy_team_name"`
-	Place         *string             `json:"place"`
-	Date          *time.Time          `json:"date"`
-	EndsData      *[]model.DataPerEnd `json:"ends_data"`
-	IsPublic      *bool               `json:"is_public"`
+	Result        *entity.Result       `json:"result"`
+	EnemyTeamName *string              `json:"enemy_team_name"`
+	Place         *string              `json:"place"`
+	Date          *time.Time           `json:"date"`
+	EndsData      *[]entity.DataPerEnd `json:"ends_data"`
+	IsPublic      *bool                `json:"is_public"`
 }
 
 type SetVisibilityRequest struct {
