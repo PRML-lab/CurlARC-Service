a1f6266cfd9a55a307323430bf4acf41c5fa8071
dyoshy <d.yoshhyy@gmail.com>
2024-12-31 19:35:38 +0900

    fix: not null constraint for is_red

--- migrations/sql/7_add-is_red-in-record.sql
+++ migrations/sql/7_add-is_red-in-record.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE "public"."records" ADD COLUMN "is_red" boolean NULL;
+ALTER TABLE "public"."records" ADD COLUMN "is_red" boolean NOT NULL DEFAULT false;
 
 -- +goose Down
 ALTER TABLE "public"."records" DROP COLUMN IF EXISTS "is_red";
